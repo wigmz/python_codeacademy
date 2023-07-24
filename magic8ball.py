5afde5eaff008821deaaa86f9c3c90b88dabcedc
wigmz <75184786+wigmz@users.noreply.github.com>
2023-07-23 20:32:23 -0700

    Create magic8ball.py

---
+++ magic8ball.py
@@ -0,0 +1,34 @@
+#a random simple game where I learned about the python control flows
+
+import random
+
+name = "Wig" 
+question = "Is it going to rain tomorrow?"
+answer = ""
+
+random_number = random.randint(1, 9)
+print(random_number)
+
+if random_number == 1:
+  answer = "Yes - definitely"
+elif random_number == 2:
+  answer = "It is decidedly so"
+elif random_number == 3:
+  answer = "Without a doubt"
+elif random_number == 4:
+  answer = "Reply hazy, try again"
+elif random_number == 5:
+  answer = "Ask again later"
+elif random_number == 6:
+  answer = "Better not tell you now"
+elif random_number == 7:
+  answer = "My sources say no"
+elif random_number == 8:
+  answer = "YOutlook not so good"
+elif random_number == 9:
+  answer = "Very doubtful"
+else:
+  answer = "Error"
+print(name + " " + "asks:" + " " + question)
+
+print("Magic 8-Ball's answer:" + " " + answer)

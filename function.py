3013687295e29bbf003038e8991f8aa00e48ba0a
Wig <75184786+wigmz@users.noreply.github.com>
2023-08-20 20:05:59 -0700

    Update function.py

--- function.py
+++ function.py
@@ -1,1 +1,25 @@
+# Defining a Function
+# A function consists of many parts, so let’s first get familiar with its core - a function definition.
+
+# Here’s an example of a function definition:
+def function_name():
+  # functions tasks go here
+
+# Key components:
+
+The def keyword indicates the beginning of a function (also known as a function header). The function header is followed by a name in snake_case format that describes the task the function performs. It’s best practice to give your functions a descriptive yet concise name.
+
+Following the function name is a pair of parenthesis ( ) that can hold input values known as parameters (more on parameters later in the lesson!). In this example function, we have no parameters.
+
+A colon : to mark the end of the function header.
+
+Lastly, we have one or more valid python statements that make up the function body (where we have our python comment).
+
+Notice we’ve indented our # function tasks go here comment. Like loops and conditionals, code inside a function must be indented to show that they are part of the function.
+
+Here is an example of a function that greets a user for our trip planning application:
+
+def trip_welcome():
+  print("Welcome to Tripcademy!") 
+  print("Let's get you to your destination.")
 

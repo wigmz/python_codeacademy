ec983da0ec4b1c966acea75cee681bf086b5617f
Wig <75184786+wigmz@users.noreply.github.com>
2023-08-22 21:39:50 -0700

    Update function.py

--- function.py
+++ function.py
@@ -123,3 +123,29 @@ print("100 dollars in US currency would give you " + str(new_zealand_exchange) +
 # Saving our values returned from a function like we did with new_zealand_exchange allows us to reuse the value (in the form of a variable) throughout the rest of the program.
 
 # When there is a result from a function that is stored in a variable, it is called a returned function value.
+
+# Multiple Returns
+# Sometimes we may want to return more than one value from a function. We can return several values by separating them with a comma. Take a look at this example of a function that allows users in our travel application to check the upcoming week’s weather (starting on Monday):
+
+weather_data = ['Sunny', 'Sunny', 'Cloudy', 'Raining', 'Snowing']
+
+def threeday_weather_report(weather):
+  first_day = " Tomorrow the weather will be " + weather[0]
+  second_day = " The following day it will be " + weather[1]
+  third_day = " Two days from now it will be " + weather[2]
+  return first_day, second_day, third_day
+
+# This function takes in a set of data in the form of a list for the upcoming week’s weather. We can get our returned function values by assigning them to variables when we call the function:
+
+monday, tuesday, wednesday = threeday_weather_report(weather_data)
+
+print(monday)
+print(tuesday)
+print(wednesday)
+
+# This will print:
+
+Tomorrow the weather will be Sunny
+The following day it will be Sunny
+Two days from now it will be Cloudy
+
